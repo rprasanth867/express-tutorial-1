b0b8ab240deebf98051ae0db55f9be62ac61a766
prudrakshula <prudrakshula@nouveau-labs.com>
2023-11-13 12:42:56 +0530

    regex and middlewares and not found

--- routes/users.js
+++ routes/users.js
@@ -2,12 +2,32 @@ const express = require("express");
 
 const router = express.Router();
 
+/*
+  It is a middleware. It calls when reached /users routes
+
+  Using for logging the request for /users routes
+*/
+router.use("/", (req, res, next) => {
+  console.log('+++ Request +++', req);
+  next();
+});
+
 router.get("/all", (req, res) => {
   res.send("Request for all users");
 });
 
-router.get("/details", (req, res) => {
-  res.send("Request for a specified user");
+// handling path params
+router.get("/details/:id", (req, res) => {
+  res.send("Request for a specified users/"+req.params.id);
+});
+
+// handling regex expression
+router.get("/search-by-key/:key([0-9]{4,6})", (req, res) => {
+  const data = {
+    key: req.params.key,
+    message: `You got results for ${req.params.key}`
+  };
+  res.send(data);
 });
 
 router.post("/create", (req, res) => {
@@ -20,6 +40,11 @@ router.put("/update", (req, res) => {
 
 router.delete("/delete", (req, res) => {
   res.send("Deleted user successfully");
+});
+
+// handling URL not found for /users routes
+router.all("*", (req, res) => {
+  res.send({status: 404, message: "URL not found"});
 });
 
 module.exports = router;
